e088b08288e95bf12b306485fecb9bfb798ff250
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-31 10:57:18 +0700

    jika FRONT_position>REAR_position, iterasi dari FRONT hingga akhir array pada metode display

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -72,5 +72,12 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "     ";
+				FRONT_position++;
+			}
+			FRONT_position = 0;
+		}
 	}
 	};
